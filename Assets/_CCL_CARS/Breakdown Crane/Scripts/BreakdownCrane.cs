3672b2d35174ec268e9d20a14f47f84b6830c87d
CruzerBlade9369 <pradiparasendrya@gmail.com>
2025-08-19 14:43:16 +0700

    Initial commit

---
+++ Assets/_CCL_CARS/Breakdown Crane/Scripts/BreakdownCrane.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace BreakdownCrane
+{
+    public static class Main
+    {
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Harmony harmony = new Harmony(modEntry.Info.Id);
+
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                modEntry.Logger.LogException($"Failed to load {modEntry.Info.DisplayName}:", ex);
+                harmony.UnpatchAll(modEntry.Info.Id);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
